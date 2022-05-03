20918b637d9c9d42ba31f78f5b5d5dec53ca7422
grzegorzplowiec <grzegorz.plowiec@gmail.com>
2022-05-03 19:15:58 +0200

    ADD: redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,29 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
-function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{ title }</h1>
-        <p>{ description }</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{ leftBtnText }</LeftButton>
-          {rightBtnText && <RightButton>{ rightBtnText }</RightButton>}
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg"></DownArrow>
       </Buttons>
     </Wrap>
@@ -31,7 +42,7 @@ const Wrap = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  background-image: ${props => `url("/images/${props.bgImage}")`}
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
 `;
 
 const ItemText = styled.div`
